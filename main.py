1df8d02329cd23eab491ed4fe95bbff99890b6ec
mv.rogozov <mv.rogozov@yandex.ru>
2023-02-03 10:49:27 +0300

    refactor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, BackgroundTasks
 from time import sleep
-from utils.wp_checker import NewsCollector
+from utils.wp_checker import NewsCollector, collect_news
 from database import db, News
 
 app = FastAPI()
@@ -20,18 +20,14 @@ def get_news():
 
 
 def sleeping_func():
-    i = 0
-    while i < 5:
-        sleep(20)
-        print(i)
-        i += 1
+    collect_news(collector, 10)
     collector.in_progress = False
 
 
-@app.get('/wakeup')
+@app.get('/collect')
 async def go_to_bed(background_tasks: BackgroundTasks):
     if collector.in_progress:
         return {"message": "collecting still in progress"}
     collector.in_progress = True
     background_tasks.add_task(sleeping_func)
-    return {"message": "sleeping_func awaked"}
+    return {"message": "collecting started"}
--- utils/wp_checker.py
+++ utils/wp_checker.py
@@ -1,8 +1,9 @@
 import csv
 import json
 import re
-
 import requests
+
+from database import db, News
 
 
 class NewsCollector():
@@ -33,10 +34,10 @@ class NewsCollector():
         cleartext = re.sub(CLEANR, '', string)
         return cleartext
 
-    def get_news(self, url: str, per_page: int) -> dict:
+    def get_news(self, url: str, per_page: int) -> list:
         """Проверка, если сайт на wordpress, собираем результат"""
         wp_api_url = f'/wp-json/wp/v2/posts/?per_page={per_page}'
-        result = {}
+        result = []
         try:
             response = requests.get(url + wp_api_url)
             if response.headers['Content-Type'].startswith(
@@ -44,32 +45,39 @@ class NewsCollector():
             ):
                 content = json.loads(response.content)
                 for post in content:
-                    result = {
+                    result.append({
                         'title': post['title']['rendered'],
                         'post': self.strip_tags(
                             post['content']['rendered']
                         ),
                         'url': url
-                    }
+                    })
         except Exception:
-            return False
+            return []
         return result
 
 
 def collect_news(collector: NewsCollector, per_page: int) -> None:
     """Собирает новости с сайтов в базу."""
+
     collector.get_urls()
     for url in collector.urls:
+        print(url)
         data = collector.get_news(url, per_page)
-        if not data:
+        if data == []:
             continue
-        #news = News.objects.all()
-        title = data['title']
-        url = data['url']
-        # if news.filter(url=url, title=title).exists():
-        #     continue
-        # News.objects.create(
-        #     title=data['title'],
-        #     news=data['post'],
-        #     url=data['url']
-        # )
+        for post in data:
+            # news = News.objects.all() # todo
+            # title = post['title']
+            # url = post['url']
+            # if news.filter(url=url, title=title).exists():
+            #     continue
+            news = News(
+                title=post['title'],
+                news=post['post'],
+                url=post['url']
+            )
+            db.add(news)
+            db.commit()
+    print('Collecting finished')
+
